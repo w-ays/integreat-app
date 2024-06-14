35f9ef7755065a52275338067eaa75d0bb99c9db
hassan <helmallouky@digex.ma>
2024-06-14 10:50:54 +0100

    ready to deploy

--- Dockerfile
+++ Dockerfile
@@ -3,20 +3,9 @@ FROM node:alpine as build
 
 WORKDIR /app
 
-
 COPY . .
 
 RUN apk add --no-cache git
-RUN cd web && yarn && yarn build --env dev_server
 
-
-FROM python:3.9-alpine
-
-# Copy the build output from the first stage
-COPY --from=build /app/web/dist/integreat /app
-
-# Expose the port the app runs on
-EXPOSE 8000
-
-# Start the Python HTTP server
-CMD ["python3", "-m", "http.server", "-d", "/app", "8000"]
+# Keep the container alive
+CMD tail -f /dev/null
--- build_and_run_events_frontend.sh
+++ build_and_run_events_frontend.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
 # Define your image and container names
-IMAGE_NAME="events-front-app"
-CONTAINER_NAME="events-front-app-container"
+IMAGE_NAME="events-ui"
+CONTAINER_NAME="events-ui-instance"
+NGINX_PROJECT_FOLDER="/var/www/events-ui"
+NGINX_CONF="/etc/nginx/conf.d/events.conf"
+
+# Check if the NGINX_PROJECT_FOLDER exists
+if [ -d "$NGINX_PROJECT_FOLDER" ]; then
+    echo "Directory exists. Deleting its contents..."
+    rm -rf $NGINX_PROJECT_FOLDER/*
+else
+    echo "Directory does not exist. Creating it..."
+    mkdir -p $NGINX_PROJECT_FOLDER
+fi
+
+# Copy the events.conf file to the conf.d directory of Nginx
+cp events.conf $NGINX_CONF
 
 # Check if the Docker image exists
 if [ -z "$(docker images -q $IMAGE_NAME)" ]; then
@@ -15,7 +29,9 @@ fi
 # Check if the Docker container exists
 if [ -z "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
     echo "Container does not exist. Running the container..."
-    docker run -d -p 8081:8000 --name $CONTAINER_NAME $IMAGE_NAME
+    docker run -d -v $NGINX_PROJECT_FOLDER:/app/web/dist/integreat --name $CONTAINER_NAME $IMAGE_NAME
+    # Go inside the container and run the commands
+    docker exec -it $CONTAINER_NAME sh -c "cd /app/web && yarn build --env dev_server"
 else
     echo "Container exists."
 
@@ -23,6 +39,9 @@ else
     if [ -z "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
         echo "Container is not running. Starting the container..."
         docker start $CONTAINER_NAME
+
+        # Go inside the container and run the commands
+        docker exec -it $CONTAINER_NAME sh -c "cd /app/web && yarn build --env dev_server"
     else
         echo "Container is already running."
     fi
